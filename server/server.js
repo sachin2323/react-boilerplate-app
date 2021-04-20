a2f6cd0be1bf470d795308189871875f5f6bb337
sachin2323 <sachinsingh328@gmail.com>
2021-04-20 23:40:07 +0530

    updated ssr

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = 8000;
 const app = express();
 
 app.use("^/$", (req, res, next) => {
-  fs.readFile(path.resolve("./build.index.html"), "utf-8", (err, data) => {
+  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
     if (err) {
       console.log(err);
       return res.status(500).send("Something went wrong");
