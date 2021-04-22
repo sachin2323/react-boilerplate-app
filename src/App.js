56e2d3370cf43b4be205c826afa64cd7934a6fa9
sachin2323 <sachinsingh328@gmail.com>
2021-04-22 11:37:54 +0530

    added pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 
