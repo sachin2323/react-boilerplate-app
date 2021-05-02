3d3dc1b81106ff2cad04de21e815953ea9c26e43
raj332710 <rajendra05071998@gmail.com>
2021-05-02 16:41:46 +0530

    test

--- server/server.js
+++ server/server.js
@@ -33,7 +33,6 @@ app.use(express.static(path.join(__dirname, '../build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'))
 })
-
 // app.use(express.static(path.resolve(__dirname, "..", "build")));
 
 app.listen(PORT, () => {
